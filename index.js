d9eea80637d0bf5b872676597ae0965cc4404ebf
Lucy Wall <lhwall@gmail.com>
2018-11-07 15:41:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Write your code in this file!
 
 function scuberGreetingForFeet(rideLength) {
-  if rideLength <= 400
-    return "This one is on me!"
-  else if rideLength > 2000 && rideLength < 2500
-    return "I will gladly take your thirty bucks."
-  else if rideLength > 2500
-   return "No can do."
+  if (rideLength <= 400) {
+    return "This one is on me!"}
+  else if (rideLength > 2000 && rideLength < 2500) {
+    return "I will gladly take your thirty bucks."}
+  else if (rideLength > 2500){
+   return "No can do."}
 }
 
 function ternaryCheckCity(input){
