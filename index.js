1a160a2d371ab923b1d3c013ca75fa7f92b73dcf
Lucy Wall <lhwall@gmail.com>
2018-11-07 15:15:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Write your code in this file!
 function scuberGreetingForFeet(rideLength){
-  if rideLength <= 400 
+  if rideLength <= 400
     return "This one's on us!"
-  else if rideLength > 2000 && rideLength < 2500 
+  else if rideLength > 2000 && rideLength < 2500
     return 30
-  else if rideLength > 2500 
+  else if rideLength > 2500
    return "Not possible"
 }
 
 function ternaryCheckCity(input){
-  
+
 }
 
 function switchOnCharmFromTip(input){
-  
+
 }
